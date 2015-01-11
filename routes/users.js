1923ebe9222e5ca09c668b987b0b7e57d679f186
Mohamed Wagdy <mohamedwagdykhorshid@gmail.com>
2015-01-11 08:13:25 +0200

    Added validation

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ module.exports = function (app, express, client) {
 
   var router = express.Router();
   var bodyParser = require('body-parser');
+  var validator = require('node-validator');
+  var cirkleValidator = require('../lib/cirkle-validator');
 
   router.use(bodyParser.json());
   router.use(bodyParser.urlencoded({extended: true}));
@@ -11,6 +13,23 @@ module.exports = function (app, express, client) {
     var email = req.body.email;
     var password = req.body.password;
     var displayname = req.body.displayname;
+
+    var userObj = {};
+    userObj.email = email;
+    userObj.password = password;
+    userObj.displayname = displayname;
+
+    // validate parameters
+    var check = validator.isObject()
+      .withRequired('email', cirkleValidator.isEmail()) 
+      .withRequired('password')
+      .withOptional('displayname');
+
+    validator.run(check, userObj, function(errorCount, errors){
+      console.log('errorCount :' + errorCount);
+      console.log('errors :' + errors);
+    });
+
     var query = 'insert into user_(email, password, displayname) values ($1, $2, $3)';
 
     console.log(query + '. Parameters: email='+ email + ', password='+ password + ', displayname='+ displayname);
@@ -18,9 +37,10 @@ module.exports = function (app, express, client) {
     client.query(query, [email, password, displayname], function(err, result) {
       if(err) {
         console.log("Error: " + err);
-        return res.send(err);
+        res.status(500);
+        res.send(err);
       }
-      res.send('OK');
+      res.status(200).end();
     });
   });
 
