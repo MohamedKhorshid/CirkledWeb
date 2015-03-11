e8c10c9ce842dc44db4e6c267bc6623d591cabf2
Mohamed Wagdy <mohamedwagdykhorshid@gmail.com>
2015-03-11 20:54:13 +0200

    build

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ module.exports = function (app, express, client) {
 
   var router = express.Router();
   var bodyParser = require('body-parser');
-  var validator = require('node-validator');
+  var validator = require('../lib/node-validator');
   var cirkleValidator = require('../lib/cirkle-validator');
 
   router.use(bodyParser.json());
@@ -21,27 +21,38 @@ module.exports = function (app, express, client) {
 
     // validate parameters
     var check = validator.isObject()
-      .withRequired('email', cirkleValidator.isEmail()) 
+      .withRequired('email', cirkleValidator.isEmail())
       .withRequired('password')
       .withOptional('displayname');
 
-    validator.run(check, userObj, function(errorCount, errors){
+    var valid = validator.run(check, userObj, function(errorCount, errors){
       console.log('errorCount :' + errorCount);
-      console.log('errors :' + errors);
+      errors.forEach(function(error){
+        console.log('error :' + error);
+        console.log('message :' + error.message);
+        console.log('parameter :' + error.parameter);
+        console.log('value :' + error.value);
+      });
+
+      res.status(400);
+      res.send(errors);
     });
 
-    var query = 'insert into user_(email, password, displayname) values ($1, $2, $3)';
+    if(valid) {
+      var query = 'insert into user_(email, password, displayname) values ($1, $2, $3)';
 
-    console.log(query + '. Parameters: email='+ email + ', password='+ password + ', displayname='+ displayname);
+      console.log(query + '. Parameters: email='+ email + ', password='+ password + ', displayname='+ displayname);
 
-    client.query(query, [email, password, displayname], function(err, result) {
-      if(err) {
-        console.log("Error: " + err);
-        res.status(500);
-        res.send(err);
-      }
-      res.status(200).end();
-    });
+      client.query(query, [email, password, displayname], function(err, result) {
+        if(err) {
+          console.log("Error: " + err);
+          res.status(500);
+          res.send(err);
+        } else {
+          res.status(200).end();
+        }
+      });
+    }
   });
 
   app.use('/', router);
